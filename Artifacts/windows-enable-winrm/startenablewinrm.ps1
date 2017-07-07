7aa28dc0e6e05cf201687be72aea6401e5e8d336
Murat MERCAN <mmercan@outlook.com>
2017-07-07 13:47:04 +1000

    Update startenablewinrm.ps1

--- Artifacts/windows-enable-winrm/startenablewinrm.ps1
+++ Artifacts/windows-enable-winrm/startenablewinrm.ps1
@@ -1,7 +1,11 @@
-$ping = New-Object System.Net.NetworkInformation.Ping
-$host1 = hostname
-$ips = $($ping.Send($host1).Address).IPAddressToString
- 
-Write-Host $ips
+#$ping = New-Object System.Net.NetworkInformation.Ping
+#$host1 = hostname
+#$ips = $($ping.Send($host1).Address).IPAddressToString
+#Write-Host $ips
 
-./enablewinrm.ps1  -hostname $ips -protocol https
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address
+ 
+$ip = $ipV4.IPV4Address.IPAddressToString
+Write-Host $ip
+
+./enablewinrm.ps1  -hostname $ip -protocol https
