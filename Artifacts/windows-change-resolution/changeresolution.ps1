2831b7d01d207005de23cc216a99f8fcf89a3d5b
Murat MERCAN <mmercan@outlook.com>
2017-07-07 15:52:33 +1000

    Update changeresolution.ps1

--- Artifacts/windows-change-resolution/changeresolution.ps1
+++ Artifacts/windows-change-resolution/changeresolution.ps1
@@ -1,1 +1,17 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+$zipfile = "https://raw.githubusercontent.com/mmercan/devtestlabs/master/Artifacts/windows-change-resolution/ChangeScreenResolution.zip"
+
+$webclient = New-Object System.Net.WebClient
+$filepath = "$pwd\ChangeScreenResolution.zip"
+$webclient.DownloadFile($zipfile,$filepath)
+echo "Saved $filename"
+
+Unzip -zipfile $filepath -outpath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+
 robocopy ".\ChangeScreenResolution" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
