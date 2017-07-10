2a2f79e86a13c959aa2753d832deaa9655896044
Murat MERCAN <mmercan@outlook.com>
2017-07-10 13:34:33 +1000

    Create addseleniumdrivers.ps1

---
+++ Artifacts/windows-create-seleniumdrivers/addseleniumdrivers.ps1
@@ -0,0 +1,15 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+$zipfile = "https://raw.githubusercontent.com/mmercan/devtestlabs/master/Artifacts/windows-create-seleniumdrivers/seleniumdrivers.zip"
+
+$webclient = New-Object System.Net.WebClient
+$filepath = "$pwd\seleniumdrivers.zip"
+$webclient.DownloadFile($zipfile,$filepath)
+echo "Saved $filename"
+
+Unzip -zipfile $filepath -outpath "C:\drivers"
