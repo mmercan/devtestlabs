5f900d0aa78966604269bc13c2042c665b2abda9
Murat MERCAN <mmercan@outlook.com>
2017-07-11 12:40:48 +1000

    Create addphantomjs.ps1

---
+++ Artifacts/windows-add-phantomjs/addphantomjs.ps1
@@ -0,0 +1,20 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+$zipfile = "https://raw.githubusercontent.com/mmercan/devtestlabs/master/Artifacts/windows-create-seleniumdrivers/phantomjs.zip"
+
+$webclient = New-Object System.Net.WebClient
+$filepath = "$pwd\phantomjs.zip"
+$webclient.DownloadFile($zipfile,$filepath)
+echo "Saved $filename"
+
+Unzip -zipfile $filepath -outpath "C:\drivers"
+
+
+$oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINESystemCurrentControlSetControlSession ManagerEnvironment' -Name PATH).Path
+$newPath=$oldPath+';C:\drivers'
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINESystemCurrentControlSetControlSession ManagerEnvironment' -Name PATH –Value $newPath
